805753356749ce7a9bab8e3abdad71e80ccbffce
Indeed <indeed@indeedblog.net>
2014-07-23 23:06:59 +0800

    add __unicode__ to existing models.

--- website/models.py
+++ website/models.py
@@ -7,10 +7,16 @@ class Genre(models.Model):
     id = models.IntegerField(verbose_name="Genre ID", primary_key=True)
     caption = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return "%d - %s" % (self.id, self.caption)
+
 
 class Language(models.Model):
     id = models.IntegerField(verbose_name="Language ID", primary_key=True)
     caption = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "%d - %s" % (self.id, self.caption)
 
 
 class Beatmap(models.Model):
@@ -21,13 +27,19 @@ class Beatmap(models.Model):
     source = models.CharField(max_length=255)
     genre = models.ForeignKey(Genre)
     language = models.ForeignKey(Language)
-    date_submitted = models.DateTimeField()
-    date_ranked = models.DateTimeField()
+    date_submitted = models.DateField()
+    date_ranked = models.DateField()
     description = models.TextField()
     difficulties = models.TextField()
+
+    def __unicode__(self):
+        return "%d %s - %s" % (self.id, self.artist, self.title)
 
 
 class Download(models.Model):
     beatmap = models.ForeignKey(Beatmap)
     server = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "[%d][%s] %s" % (self.beatmap_id, self.server, self.url)
