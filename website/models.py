0a5b4457d038351bde82134f2ddd28963ca01966
Indeed <indeed@indeedblog.net>
2014-07-23 22:19:36 +0800

    implement basic models.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    id = models.IntegerField(verbose_name="Genre ID", primary_key=True)
+    caption = models.CharField(max_length=255)
+
+
+class Language(models.Model):
+    id = models.IntegerField(verbose_name="Language ID", primary_key=True)
+    caption = models.CharField(max_length=255)
+
+
+class Beatmap(models.Model):
+    id = models.IntegerField(verbose_name="Beatmap ID", primary_key=True)
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    creator = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    genre = models.ForeignKey(Genre)
+    language = models.ForeignKey(Language)
+    date_submitted = models.DateTimeField()
+    date_ranked = models.DateTimeField()
+    description = models.TextField()
+    difficulties = models.TextField()
+
+
+class Download(models.Model):
+    beatmap = models.ForeignKey(Beatmap)
+    server = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
