2d57bc17de49ba46fa0d75d0e70f3e85741d3a79
Indeed <indeed@indeedblog.net>
2014-07-23 23:31:16 +0800

    register models.

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from models import Genre, Language, Beatmap, DownloadServer, Download
+
 # Register your models here.
+
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Beatmap)
+admin.site.register(DownloadServer)
+admin.site.register(Download)
