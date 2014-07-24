f69632c5b9e7d6cf84a45846374b84858bdab784
Indeed <indeed@indeedblog.net>
2014-07-24 17:21:42 +0800

    add website app to root urls config.

--- osubeatmaps/urls.py
+++ osubeatmaps/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'osubeatmaps.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('website.urls', namespace="website")),
     url(r'^admin/', include(admin.site.urls)),
+
 )
