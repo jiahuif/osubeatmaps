e0a11fb24d46f046691caed3a235dd6e8694deb4
Indeed <indeed@indeedblog.net>
2014-07-24 19:22:43 +0800

    implement views.

---
+++ website/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+
+from website import views
+
+urlpatterns = patterns('',
+                       url(r'^$', views.IndexView.as_view(), name='index'),
+                       url(r'^listing/(?P<genre_id>\d+)/(?P<language_id>\d+)/(?P<page_id>\d+)$', views.listing,
+                           name='listing'),
+                       url(r'^disclaimer/$', views.DisclaimerView.as_view(), name='disclaimer'),
+)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views import generic
 # Create your views here.
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'website/index.html'
+
+
+def listing(request):
+    return render(request, 'website/listing.html')
+
+
+class DisclaimerView(generic.TemplateView):
+    template_name = 'website/disclaimer.html'
