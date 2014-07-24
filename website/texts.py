f4568fc1545e5c98be308608f0668e816f373501
Indeed <indeed@indeedblog.net>
2014-07-24 22:34:31 +0800

    list texts for translation.

---
+++ website/texts.py
@@ -0,0 +1,20 @@
+from django.utils.translation import ugettext_lazy as _
+# This file contains text resources that may appear in genre or language field of beatmap records
+# Mainly for translation
+
+# Translators: Genres
+GENRE_ANY = _("Any")
+GENRE_UNSPECIFIED = _("Unspecified")
+GENRE_VIDEO_GAME = _("Video Game")
+GENRE_ANIME = _("Anime")
+GENRE_ROCK = _("Rock")
+GENRE_POP = _("Pop")
+GENRE_OTHER = _("Other")
+GENRE_NOVELTY = _("Novelty")
+GENRE_HIP_HOP = _("Hip Hop")
+GENRE_TECHNO = _("Techno")
+
+# Translators: Languages
+LANGUAGE_ANY = _("Any")
+LANGUAGE_UNSPECIFIED = _("Other")
+LANGUAGE_INSTRUMENTAL = _("Instrumental")
