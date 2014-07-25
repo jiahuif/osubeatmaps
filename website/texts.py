023171f0d923295fcb74b0de75b9e4f3144291f9
Indeed <indeed@indeedblog.net>
2014-07-25 17:03:52 +0800

    add comments to texts.

--- website/texts.py
+++ website/texts.py
@@ -4,17 +4,30 @@ from django.utils.translation import ugettext_lazy as _
 
 # Translators: Genres
 GENRE_ANY = _("Any")
+# Translators: Genres
 GENRE_UNSPECIFIED = _("Unspecified")
+# Translators: Genres
 GENRE_VIDEO_GAME = _("Video Game")
+# Translators: Genres
 GENRE_ANIME = _("Anime")
+# Translators: Genres
 GENRE_ROCK = _("Rock")
+# Translators: Genres
 GENRE_POP = _("Pop")
+# Translators: Genres
 GENRE_OTHER = _("Other")
+# Translators: Genres
 GENRE_NOVELTY = _("Novelty")
+# Translators: Genres
 GENRE_HIP_HOP = _("Hip Hop")
+# Translators: Genres
 GENRE_TECHNO = _("Techno")
 
 # Translators: Languages
 LANGUAGE_ANY = _("Any")
+# Translators: Languages
+LANGUAGE_CHINESE = _("Chinese")
+# Translators: Languages
 LANGUAGE_UNSPECIFIED = _("Other")
+# Translators: Languages
 LANGUAGE_INSTRUMENTAL = _("Instrumental")
